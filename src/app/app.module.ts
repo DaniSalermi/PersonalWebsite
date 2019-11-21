dcf09b130cba1561d260a6eb19a627958e869d08
Daniela Salermi <salermi22@gmail.com>
2019-11-20 21:42:11 -0300

    menu animation is working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ReusableModule } from './modules/reusable/reusable.module';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ReusableModule } from "./modules/reusable/reusable.module";
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/modules/reusable/reusable.module.ts
+++ src/app/modules/reusable/reusable.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { RouterModule } from "@angular/router";
+import { MenuComponent } from "./menu/menu.component";
+import { SafePipe } from "src/app/pipes/safe.pipe";
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, MenuComponent, SafePipe],
   imports: [CommonModule, RouterModule],
-  exports: [HeaderComponent, FooterComponent]
+  exports: [HeaderComponent, FooterComponent, MenuComponent]
 })
 export class ReusableModule {}
